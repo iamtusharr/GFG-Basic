a98e0056a11c64fb8e1fb4ab62ab2b556d2bcacf
Tushar Goyal <94983995+iamtusharr@users.noreply.github.com>
2022-07-10 17:29:56 +0530

    Create Find Number Of Numbers.java

---
+++ Find Number Of Numbers.java
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+import java.util.*;
+class Numbers{
+public static void main(String[] args)
+{
+	Scanner sc=new Scanner(System.in);
+	int t=sc.nextInt();
+	while(t-->0)
+	{
+		int n=sc.nextInt();
+		int[] a=new int[n];
+		for(int i = 0; i < n; i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int k=sc.nextInt();
+		GfG g=new GfG();
+		System.out.println(g.num(a,n,k));
+	}
+}
+}// } Driver Code Ends
+
+
+/*Complete the Function below*/
+class GfG
+{
+          public static int num(int a[], int n, int k)
+            {
+                
+                int count_k = 0;
+                int temp = 0;
+                int rem = 0;
+                 for(int i = 0 ; i< n ;i++)
+                 {
+                    temp = a[i];
+                     while(temp > 0)
+                     {
+                         rem = temp % 10 ;
+                         if(rem == k)
+                         {
+                             count_k++;
+                         }
+                         temp = temp / 10;
+                     }
+                     
+                 }
+                 return count_k;
+            }
+}
