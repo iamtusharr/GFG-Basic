1016c2ddb3e90dda3f1b1c343cf645575407c8b2
Tushar Goyal <94983995+iamtusharr@users.noreply.github.com>
2022-07-10 14:03:35 +0530

    Create Pythagorean Triplet.java

---
+++ Pythagorean Triplet.java
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            boolean ans = new Solution().checkTriplet(arr, n);
+            System.out.println(ans ? "Yes" : "No");
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    boolean checkTriplet(int[] arr, int n) 
+    {
+        for(int i = 0 ; i < n ; i++)
+        {
+            for( int j = i+1 ; j < n ; j++ )
+            {
+                for(int k = j+1 ; k < n ; k++)
+                {
+                    int a = arr[i] * arr[i];
+                    int b = arr[j] * arr[j];
+                    int c = arr[k] * arr[k];
+                    if(a == b + c || b == a + c || c == a + b)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
