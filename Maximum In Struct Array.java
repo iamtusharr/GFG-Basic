0759da21b05d452f00e724a6c7166b668f949789
Tushar Goyal <94983995+iamtusharr@users.noreply.github.com>
2022-07-10 14:12:26 +0530

    Create Maximum In Struct Array.java

---
+++ Maximum In Struct Array.java
@@ -0,0 +1,78 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+
+class Height
+{
+    int feet;
+    int inches;
+    
+      public Height(int ft, int inc)
+      {
+           feet = ft;
+           inches = inc;
+      }
+}
+
+class maximum
+{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            Height arr[] = new Height[n];
+            for(int i = 0; i < n; i++)
+            {
+                int temp1 = sc.nextInt();
+                int temp2 = sc.nextInt();
+                arr[i] = new Height(temp1, temp2);
+              
+            }
+            GfG gfg = new GfG();
+            
+            int res = gfg.findMax(arr, n);
+            System.out.println(res);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+Class of the element of the array is as
+class Height {
+	int feet;
+	int inches;
+	public Height(int ft, int inc)
+	{
+	    feet = ft;
+	    inches = inc;
+	}
+}
+*/
+// function must return the maximum Height
+// return the height in inches
+class GfG
+{
+    public static int findMax(Height arr[], int n)
+    {
+        int max = Integer.MIN_VALUE;
+        int temp = 0;
+        for(int i = 0 ; i < n ; i++)
+        {
+            int h1 , h2;
+            h1 = arr[i].feet * 12;
+            h2 = arr[i].inches;
+            temp = h1 + h2;
+            if(max < temp)
+            {
+                max = temp;
+            }
+        }
+        return max;
+    }
+    
+}
