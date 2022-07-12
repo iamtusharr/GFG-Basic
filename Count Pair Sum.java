cb7f50440c823b03f11dbe37b8376b2a45360ef9
Tushar Goyal <94983995+iamtusharr@users.noreply.github.com>
2022-07-12 14:42:01 +0530

    Create Count Pair Sum.java

---
+++ Count Pair Sum.java
@@ -0,0 +1,93 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+
+
+ // } Driver Code Ends
+//User function Template for Java
+
+
+class Solution
+{ 
+  
+    // function to count all pairs  
+    // from both the sorted arrays  
+    // whose sum is equal to a given  
+    // value 
+    static int countPairs(int arr1[],int arr2[], int M, int N, int x) 
+    {
+        int count = 0;
+        for(int i = 0 ; i < M ; i++)
+        {
+            int temp = x - arr1[i];
+            if(search(arr2,temp))
+            {
+                count++;
+            }
+        }
+        return count;
+        
+        
+    }
+    static boolean search(int arr2[] , int temp )
+    {
+        int n = arr2.length;
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(arr2[i] == temp)
+            {
+                return true;
+            }
+            
+        }
+        return false;
+    }
+}
+
+// { Driver Code Starts.
+
+// Driver class
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		// looping through all testcases
+		while(testcases-- > 0){
+		    //int n=Integer.parseInt(br.readLine());
+		    String line1 = br.readLine();
+		    String[] a1 = line1.trim().split("\\s+");
+		    int m=Integer.parseInt(a1[0]);
+		    int n=Integer.parseInt(a1[1]);
+		    String line2 = br.readLine();
+		    String[] a2 = line2.trim().split("\\s+");
+		    int a[]=new int[m];
+		    for(int i=0;i<m;i++)
+		    {
+		        a[i]=Integer.parseInt(a2[i]);
+		    }
+		    String line3 = br.readLine();
+		    String[] a3 = line3.trim().split("\\s+");
+		    int b[]=new int[n];
+		    for(int i=0;i<n;i++)
+		    {
+		        b[i]=Integer.parseInt(a3[i]);
+		    }
+		    int x=Integer.parseInt(br.readLine());
+		    Solution ob=new Solution();
+		    int ans=ob.countPairs(a,b,m,n,x);
+		    System.out.println(ans);
+		    
+		}
+	}
+}
+  // } Driver Code Ends
