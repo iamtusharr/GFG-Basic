e2192b248b5218609c8016a317360f2eb37c84d4
Tushar Goyal <94983995+iamtusharr@users.noreply.github.com>
2022-07-13 22:43:11 +0530

    Create Professor And Parties.java

---
+++ Professor And Parties.java
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.PartyType(a, n));
+            
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+
+    public static String PartyType( long a[], int n)
+    {
+        String str = "GIRLS";
+        Arrays.sort(a);
+        for(int i = 0 ; i < n-1 ; i++)
+        {
+            if(a[i] == a[i+1])
+            {
+                str = "BOYS";
+                return str;
+            }
+        }
+        return str;
+        
+    }
+}
